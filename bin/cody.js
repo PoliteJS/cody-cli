88b35ca72d9f7a3fc68380652c547ea331ae2d13
Marco Pegoraro <marco.pegoraro@gmail.com>
2015-11-01 10:49:50 +0100

    host builds in home folder

--- bin/cody.js
+++ bin/cody.js
@@ -1,5 +1,9 @@
 #! /usr/bin/env node
 
+function getUserHome() {
+    return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+}
+
 var path = require('path');
 var md5 = require('md5');
 var cody = require('../lib/cody');
@@ -7,7 +11,7 @@ var run = require('../lib/run');
 
 var projectHash = md5(process.cwd());
 var projectSrc = process.cwd();
-var projectDest = path.join(__dirname, '..', 'projects', projectHash);
+var projectDest = path.join(getUserHome(), '.cody-cli', 'projects', projectHash);
 var projectBuild = path.join(projectDest, 'build');
 
 cody.init({
@@ -19,14 +23,16 @@ cody.init({
 });
 
 run.script('checkup').then(function() {
-    var tasks = []; 
+    var tasks = [];
+    var tasks1 = [];
 
     // dev only
-    // run.gulp('transpile-js');return;
+    // run.gulp('copy-html');return;
 
     tasks.push(run.gulp('copy-html'));
     tasks.push(run.gulp('copy-css'));
     tasks.push(run.gulp('copy-js'));
+
     tasks.push(run.gulp('transpile-less'));
     tasks.push(run.gulp('transpile-scss'));
     tasks.push(run.gulp('transpile-js'));
@@ -34,8 +40,8 @@ run.script('checkup').then(function() {
     Promise.all(tasks).then(function() {
         run.gulp('lint-js');
         run.gulp('lint-css');
-
         run.gulp('watch');
+        
         run.service('http-server');
     });
 });
